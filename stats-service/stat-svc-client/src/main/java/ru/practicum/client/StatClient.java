d8a08007243c38f9405fad1cfde7eeba36005ed1
LacusFaustus <lacusfaustus@gmail.com>
2026-05-23 09:35:46 +0300

    feat: add Spring Cloud infrastructure and Feign client

--- stats-service/stat-svc-client/src/main/java/ru/practicum/client/StatClient.java
+++ stats-service/stat-svc-client/src/main/java/ru/practicum/client/StatClient.java
@@ -1,27 +1,58 @@
 package ru.practicum.client;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestClient;
 import ru.practicum.dto.NewEndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 public class StatClient {
+    private final DiscoveryClient discoveryClient;
     private final RestClient restClient;
+    private final RetryTemplate retryTemplate;
+    private final String statsServiceId;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public StatClient(String baseUrl) {
-        this.restClient = RestClient.create(baseUrl);
+    public StatClient(DiscoveryClient discoveryClient, RestClient restClient,
+                      RetryTemplate retryTemplate, String statsServiceId) {
+        this.discoveryClient = discoveryClient;
+        this.restClient = restClient;
+        this.retryTemplate = retryTemplate;
+        this.statsServiceId = statsServiceId;
+    }
+
+    private URI makeUri(String path) {
+        ServiceInstance instance = retryTemplate.execute(context -> getInstance());
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort() + path);
+    }
+
+    private ServiceInstance getInstance() {
+        try {
+            List<ServiceInstance> instances = discoveryClient.getInstances(statsServiceId);
+            if (instances.isEmpty()) {
+                throw new RuntimeException("No instances found for service: " + statsServiceId);
+            }
+            return instances.getFirst();
+        } catch (Exception exception) {
+            throw new RuntimeException("Ошибка обнаружения адреса сервиса статистики с id: " + statsServiceId, exception);
+        }
     }
 
     public void saveHit(NewEndpointHitDto hitDto) {
         try {
+            URI uri = makeUri("/hit");
             ResponseEntity<Void> response = restClient.post()
-                    .uri("/hit")
+                    .uri(uri)
                     .body(hitDto)
                     .retrieve()
                     .toBodilessEntity();
@@ -29,7 +60,9 @@ public class StatClient {
             if (response.getStatusCode().isError()) {
                 throw new RuntimeException("Failed to save hit: " + response.getStatusCode());
             }
+            log.debug("Successfully saved hit to stats service");
         } catch (Exception e) {
+            log.error("Error while saving hit to stats service: {}", e.getMessage());
             throw new RuntimeException("Error while saving hit to stats service", e);
         }
     }
@@ -39,6 +72,8 @@ public class StatClient {
         try {
             String startStr = start.format(FORMATTER);
             String endStr = end.format(FORMATTER);
+
+            URI uri = makeUri("/stats");
 
             ResponseEntity<ViewStatsDto[]> response = restClient.get()
                     .uri(uriBuilder -> {
@@ -58,6 +93,7 @@ public class StatClient {
 
             return Arrays.asList(response.getBody() != null ? response.getBody() : new ViewStatsDto[0]);
         } catch (Exception e) {
+            log.error("Error while getting stats from stats service: {}", e.getMessage());
             throw new RuntimeException("Error while getting stats from stats service", e);
         }
     }
